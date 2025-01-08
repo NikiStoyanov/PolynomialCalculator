c1c435fd9dc409fe617af8d2ba396e316820560b
Nikolay Stoyanov <nikistoyanov2005@gmail.com>
2025-01-08 08:53:17 +0200

    Add project files.

---
+++ PolynomialCalculator.cpp
@@ -0,0 +1,22 @@
+/**
+* 
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter Semester 2024/2025
+* 
+* @author Nikolay Stoyanov
+* @idnumber 2MI0600435
+* @compiler VC
+* 
+* Description
+* 
+*/
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
